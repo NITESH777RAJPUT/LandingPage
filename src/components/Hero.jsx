af518039cdbae8989d5d3f2ef82c51826e4568c1
NITESH777RAJPUT <nitesh02006@gmail.com>
2026-01-28 01:18:55 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,3 @@
-import heroVideo from "/hero-video.mp4";
-
 const NAVBAR_HEIGHT = 86;
 
 const Hero = () => {
@@ -8,21 +6,22 @@ const Hero = () => {
             className="relative w-full overflow-hidden"
             style={{
                 paddingTop: `${NAVBAR_HEIGHT}px`,
-                height: `100vh`,
+                height: "100vh",
             }}
         >
             {/* VIDEO */}
             <video
-                src={heroVideo}
+                src="/hero-video.mp4"
                 autoPlay
                 muted
                 loop
                 playsInline
+                preload="metadata"
                 className="absolute top-[86px] left-0 w-full h-[calc(100%-86px)] object-cover"
             />
 
             {/* OVERLAY */}
-            <div className="absolute top-[86px] left-0 w-full h-[calc(100%-86px)] bg-black/20"></div>
+            <div className="absolute top-[86px] left-0 w-full h-[calc(100%-86px)] bg-black/20" />
         </section>
     );
 };
